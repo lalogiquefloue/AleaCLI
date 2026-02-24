107a0bc7543397d9beab02ced83b0e5453cf13c1
Carl Thibault <lalogiquefloue@gmail.com>
2026-02-24 15:48:24 -0500

    fix typos in mock engine 2

--- src/engines/mock_engine_2.c
+++ src/engines/mock_engine_2.c
@@ -4,8 +4,8 @@
 static uint32_t mock_engine_1_state = 1;
 
 void mock_engine_2_setup(uint32_t seed) {
-    mock_engine_1_state = seed;
-    printf("mock_engine_1_state=%d\n", mock_engine_1_state);
+    mock_engine_2_state = seed;
+    printf("mock_engine_2_state=%d\n", mock_engine_2_state);
 }
 
 uint32_t mock_engine_2(void) {
